3cd1bd077b15be2fa1574c8e5570edc79e73dd1d
Arkadiusz Benedykt <abenedykt@rekord.com.pl>
2016-04-18 09:58:40 +0200

    first commit with guerilla ts gen tool

---
+++ src/Extensions/PopertyInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace AB.TsGen.Extensions
+{
+    public static class PopertyInfoExtensions
+    {
+        public static string AsTypeScriptType(this PropertyInfo property)
+        {
+            if (property.PropertyType == typeof(string))
+            {
+                return "string";
+            }
+            if (property.PropertyType == typeof(int))
+            {
+                return "number";
+            }
+            if (property.PropertyType == typeof(DateTime))
+            {
+                return "string";
+            }
+            if (property.PropertyType.IsClass)
+            {
+                return property.PropertyType.Name;
+            }
+            return "any";
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using AB.TsGen.Extensions;
+
+namespace AB.TsGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = args[0];
+            var outPath = args[1];
+            Console.WriteLine($"assembly : {assembly}");
+            Console.WriteLine($"out path : {outPath}");
+            var dtos = Load(assembly).GetTypes()
+                .Where(NotAbstract)
+                .Where(IsClass)
+                .Where(EndsWithDto);
+
+            var ts = new StringBuilder();
+            foreach (var dto in dtos)
+            {
+                ts.Append($"export class {dto.Name}\r\n");
+                ts.Append("{\r\n");
+
+                foreach (var property in dto.GetProperties())
+                {
+                    ts.Append($"\tpublic {property.Name}: {property.AsTypeScriptType()};\r\n");
+                }
+                ts.Append("}\r\n");
+                ts.Append("\r\n");
+            }
+            
+            File.WriteAllText(outPath, ts.ToString());
+
+            Console.WriteLine("done :D");
+        }
+
+        private static Assembly Load(string assemblyPath)
+        {
+            return Assembly.LoadFrom(assemblyPath);
+        }
+
+        private static bool EndsWithDto(Type type)
+        {
+            return type.Name.EndsWith("Dto");
+        }
+
+        private static bool IsClass(Type type)
+        {
+            return type.IsClass;
+        }
+
+        private static bool NotAbstract(Type type)
+        {
+            return type.IsAbstract == false;
+        }
+    }
+}
